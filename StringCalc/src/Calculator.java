afc3457ce2fee8b88437b7fadb93b1ccd1710a31
keshri-rakesh <keshrirakesh6@gmail.com>
2021-08-15 22:23:23 +0530

    Test 3 Pass

--- StringCalc/src/Calculator.java
+++ StringCalc/src/Calculator.java
@@ -1,10 +1,16 @@
-
 public class Calculator {
 	public int calculate(String userinput) {
+		String[] numbers = userinput.split(",");
+		
 		if(checkisEmpty(userinput)) {
 			return 0;
 		}
-		return stringtoInt(userinput);
+		if(userinput.length() == 1) {
+			return stringtoInt(userinput);
+		}
+		else {
+			return stringtoInt(numbers[0]) + stringtoInt(numbers[1]);
+		}
 	}
 	
 	private boolean checkisEmpty(String userinput) {
