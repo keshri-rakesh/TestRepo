7fbd52793dc7d5137df87cf8ef1bac0634af9303
keshri-rakesh <keshrirakesh6@gmail.com>
2021-08-15 22:07:29 +0530

    Test 2 Failed

--- StringCalc/src/TestCalc.java
+++ StringCalc/src/TestCalc.java
@@ -5,8 +5,15 @@ import org.testng.annotations.Test;
 @Test
 public class TestCalc {
 	
+	//Test1 return zero for empty string
 	public void test1emptystring() {
 		Calculator calc = new Calculator();
 		assertEquals(calc.calculate(""), 0);
+	}
+	
+	//Test2 return value for single input
+	public void test2returnsinglevalue() {
+		Calculator calc = new Calculator();
+		assertEquals(calc.calculate("1"), 1);
 	}
 }
