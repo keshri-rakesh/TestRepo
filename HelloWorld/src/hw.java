2c523e3445d6ce9281b1cb666a0e3c4fb8ae992c
keshri-rakesh <keshrirakesh6@gmail.com>
2021-08-15 20:48:17 +0530

    check git commit

--- HelloWorld/src/hw.java
+++ HelloWorld/src/hw.java
@@ -4,6 +4,8 @@ public class hw {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println("Git Check");
 	}
 
 }
