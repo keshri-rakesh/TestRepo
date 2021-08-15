9503a8831587f9e90395471a30fb2e97391461a0
keshri-rakesh <keshrirakesh6@gmail.com>
2021-08-15 22:27:25 +0530

    Test 3 Refactor Code

--- StringCalc/src/Calculator.java
+++ StringCalc/src/Calculator.java
@@ -9,8 +9,12 @@ public class Calculator {
 			return stringtoInt(userinput);
 		}
 		else {
-			return stringtoInt(numbers[0]) + stringtoInt(numbers[1]);
+			return add(numbers[0],numbers[1]);
 		}
+	}
+	
+	private int add(String input1, String input2) {
+		return stringtoInt(input1) + stringtoInt(input2);
 	}
 	
 	private boolean checkisEmpty(String userinput) {
