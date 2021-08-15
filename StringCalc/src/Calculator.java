aacdbe5eb24e5b59b794358ddafbf963f43b878e
keshri-rakesh <keshrirakesh6@gmail.com>
2021-08-15 22:15:05 +0530

    Test 2 Refactor Code

--- StringCalc/src/Calculator.java
+++ StringCalc/src/Calculator.java
@@ -1,10 +1,17 @@
 
 public class Calculator {
 	public int calculate(String userinput) {
-		if(userinput.isEmpty()) {
+		if(checkisEmpty(userinput)) {
 			return 0;
 		}
+		return stringtoInt(userinput);
+	}
+	
+	private boolean checkisEmpty(String userinput) {
+		return userinput.isEmpty();
+	}
+	
+	private int stringtoInt(String userinput) {
 		return Integer.parseInt(userinput);
-		
 	}
 }
--- StringCalc/src/TestCalc.java
+++ StringCalc/src/TestCalc.java
@@ -5,15 +5,20 @@ import org.testng.annotations.Test;
 @Test
 public class TestCalc {
 	
+	private Calculator calc;
+	
+	public void init() {
+		calc = new Calculator();
+	}
+	
 	//Test1 return zero for empty string
 	public void test1emptystring() {
-		Calculator calc = new Calculator();
+		
 		assertEquals(calc.calculate(""), 0);
 	}
 	
 	//Test2 return value for single input
 	public void test2returnsinglevalue() {
-		Calculator calc = new Calculator();
 		assertEquals(calc.calculate("1"), 1);
 	}
 }
