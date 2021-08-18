447deeffc2580c5ae94ade8fc40eb103e6408c65
keshri-rakesh <keshrirakesh6@gmail.com>
2021-08-18 13:45:22 +0530

    Test to count add method invoked

--- StringCalc/src/Calculator.java
+++ StringCalc/src/Calculator.java
@@ -1,41 +1,56 @@
 public class Calculator {
-	
-	private final String delimiters = ",|\n";
-	
+	static int counter=0;
 	public int calculate(String userinput) throws Exception {
-		String[] numbers = userinput.split(delimiters);
-		
 		if(checkisEmpty(userinput)) {
 			return 0;
 		}
-		if(userinput.length() == 1) {
-			return stringtoInt(userinput);
-		}
 		else {
+			String delimiters = ",|\n|;";
+			
+			if(userinput.matches("//(.*)\n(.*)")){
+				delimiters = Character.toString(userinput.charAt(2));
+				userinput = userinput.substring(4);
+			}
+			String[] numbers = splitNumbers(userinput, delimiters);
 			return add(numbers);
 		}
 	}
 	
-	private void checkfornegativeinputs(String numbers[]) throws Exception { //to check negative inputs
-		for(String count : numbers) {
+	private static String[] splitNumbers(String numbers, String divider) {
+		return numbers.split(divider);
+	}
+	
+	private void checkfornegativeinputs(String numbers[]) throws Exception {
+		String negString="";
+		for(String count : numbers)
 			if(stringtoInt(count)<0) {
-				throw new Exception("Negatives not allowed");
+				if(negString.equals(""))
+					negString = count;
+				else{
+					negString+= ("," +count);
+				}
 			}
-		}
+			if(!negString.equals("")){
+				throw new Exception("Negatives not allowed \n Negative numbers: "+negString);
+			}
 	}
 	
 	private int add(String numbers[]) throws Exception { //add method to handle multiple inputs
-		
+		int sum=0;
 		checkfornegativeinputs(numbers);
 		
-		int sum=0;
 		for(String count : numbers) {
 			if(stringtoInt(count) >=1000) {
 				continue;
 			}
 			sum+= stringtoInt(count);
 		}
+		counter++;
 		return sum;
+	}
+	
+	public static int getCalledCount(){
+		return counter;
 	}
 	
 	private boolean checkisEmpty(String userinput) {
--- StringCalc/src/TestCalc.java
+++ StringCalc/src/TestCalc.java
@@ -1,13 +1,14 @@
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.testng.internal.ExpectedExceptionsHolder;
 
 @Test
 public class TestCalc {
 	
 	private Calculator calc;
 	
+	@BeforeTest
 	public void init() {
 		calc = new Calculator();
 	}
@@ -21,31 +22,50 @@ public class TestCalc {
 	//Test 2 return value for single input
 	public void test2returnsinglevalue() throws Exception {
 		assertEquals(calc.calculate("1"), 1);
+		assertEquals(calc.calculate("5"), 5);
 	}
 	
 	//Test 3 sum two values with comma delimited
 	public void test3returnsumoftwoinput() throws Exception {
-		assertEquals(calc.calculate("1,2"), 3);
+		assertEquals(calc.calculate("//;\n1;2"), 3);
 	}
 	
 	//Test 4 sum two values with newline delimited
 	public void test4returnssumwithnewlinedelimiter() throws Exception {
 		assertEquals(calc.calculate("1\n2"), 3);
+		assertEquals(calc.calculate("1;2"), 3);
+		assertEquals(calc.calculate("1,2"), 3);
 	}
 	
 	//Test 5 sum three numbers anyway delimited
 	public void test5threenumsum() throws Exception {
-		assertEquals(calc.calculate("1,2\n3"), 6);
+		assertEquals(calc.calculate("1,2;3"), 6);
+		assertEquals(calc.calculate("1\n2,3"), 6);
+		assertEquals(calc.calculate("1;2\n3"), 6);
 	}
 	
-	//Test 6 throw Exception for negative input (recommit)
+	//Test 6 throw Exception for negative input 
 	@Test(expectedExceptions = Exception.class)
 	public void test6throwsexceptionfornegativeinputs() throws Exception {
 		calc.calculate("-1");
+		calc.calculate("-3");
+		calc.calculate("-5");
 	}
 	
 	//Test 7 ignore numbers greater than 1000
 	public void test7ignorenumbergreaterthan1000() throws Exception {
 		assertEquals(calc.calculate("1000,2"), 2);
+		assertEquals(calc.calculate("1998,9"), 9);
+	}
+	
+	//Test 8 multiple negatives
+	@Test(expectedExceptions = Exception.class)
+	public void test8formultiplenegatives() throws Exception{
+		calc.calculate("-1,-2");
+	}
+	
+	//Test9 no of times add method invoked
+	public void test9addmethodinvokedcount() throws Exception {
+		assertEquals(calc.getCalledCount(), 11);
 	}
 }
